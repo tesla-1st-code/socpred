45d5a49175c86921f3a9c503b4173874673a5b79
vai <firstcode2017@gmail.com>
2018-12-17 23:34:10 +0700

    init project

---
+++ app.ts
@@ -0,0 +1,32 @@
+import "reflect-metadata";
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as routingControllers from "routing-controllers";
+
+const ENV = require("./env.json")[process.env.NODE_ENV || "development"];
+const CORS = (req, res, next) => {
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-type, Accept, X-Token');
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+}
+
+let app = express();
+
+app.use(CORS);
+app.use(bodyParser.json({ limit: '50mb'}));
+app.use(bodyParser.urlencoded({ limit: '50mb' , extended: false }));
+
+routingControllers.useExpressServer(app, {
+    controllers: [__dirname + "/controllers/*.js"]
+});
+
+app.listen(ENV["server_port"], err => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    console.log(`Socpred server is running on ${ENV["server_port"]}`);
+});
